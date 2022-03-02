9ac7046bed9be0d527611a3a10b95b1cabbdb426
dev-khalid <khalid.hossain.akash@gmail.com>
2022-03-02 22:21:47 +0600

    leetcode sprial order matrix

--- checking character array.cpp
+++ checking character array.cpp
@@ -9,19 +9,22 @@ int length(char input[]) {
     return count;
 }
 
+void prefix(char input[]) {
+    char result[100];
+    for(int i = 0; input[i] != '\0'; i++) {
+        result[i] = input[i];
+        result[i+1]= '\0';
+        cout << result << endl;
+    }
+}
+
+//got my head wrap around with vector and string . two basic building block of c++ stl to start of with .
+
 int main() {
-    char a[100];
-    char b[200];
-    char _[1];
-    cin >> a;
-    cin.getline(_,1);
-    cout << "it's a" << a << endl;
-    cout << "enter b" << endl;
-    cin.getline(b,200);
-    //a[4]='\0'; //the null character that is used as terminator or reading via cout.
-
-
-    cout << b;
+    vector<int> b;
+    b.push_back(10);
+    b.push_back(20);
+    for(int i = 0; i < b.size(); i++) cout << b.at(i);
 
     return 0;
 }
