07c7aa5f295b7396e1642741df59426b74127521
dev-khalid <khalid.hossain.akash@gmail.com>
2022-03-02 21:05:27 +0600

    more about character array

--- checking character array.cpp
+++ checking character array.cpp
@@ -1,9 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int length(char input[]) {
+    int count = 0;
+    for(int i = 0; input[i] != '\0'; i++) {
+        count++;
+    }
+    return count;
+}
+
 int main() {
     char a[100];
-    cin>>a;
-    cout << a << endl;
+    char b[200];
+    char _[1];
+    cin >> a;
+    cin.getline(_,1);
+    cout << "it's a" << a << endl;
+    cout << "enter b" << endl;
+    cin.getline(b,200);
+    //a[4]='\0'; //the null character that is used as terminator or reading via cout.
+
+
+    cout << b;
+
     return 0;
 }
