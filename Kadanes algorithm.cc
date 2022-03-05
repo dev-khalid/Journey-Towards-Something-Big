f7942be96dc38ff3a3cc3f7cf7316fd27e965607
dev-khalid <khalid.hossain.akash@gmail.com>
2022-03-05 09:28:01 +0600

    kadanes algorithm for finding maximum subarray sum

---
+++ Kadanes algorithm.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int kadanes_algorithm(int *a,int n) {
+    int max_sum = a[0];//this will help to find max sum in an array where all elements are negative
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        sum+=a[i];
+
+        max_sum = max(max_sum,sum);
+    }
+    return max_sum;
+}
+int main() {
+    int a[] = {-1,-2,-3};
+
+    cout << kadanes_algorithm(a,3);
+}
