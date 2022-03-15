2137c927a31fa518193103857cb0bf2c43457700
dev-khalid <khalid.hossain.akash@gmail.com>
2022-03-15 08:12:00 +0600

    L-oj 1088

--- light oj/1088 - Points in Segments.cpp
+++ light oj/1088 - Points in Segments.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
+int arr[100005];
 int main()
 {
     int t;
@@ -10,104 +11,33 @@ int main()
     {
         int n,q;
         cin >> n >> q;
-        vector<ll> query;
-        vector<ll> arr(n);
-        vector<pair<ll,int>> queries;
-        unordered_map<ll,int> rightDp; //storing how many elements are greater then Qi
-        unordered_map<ll,int> leftDp; //storing how many elements are greater then Qi
-        unordered_map<ll,int> fr;
+        vector<pair<ll,ll>> queries;
         for(int i = 0; i < n; i++)
         {
             cin >> arr[i];
-            fr[arr[i]]++;
         }
+        sort(arr,arr+n);
+        printf("Case %d:\n",cse);
         for(int i = 0; i < q; i++)
         {
-            int qi,qj;
+            ll qi,qj;
             cin >> qi>> qj;
-            queries.push_back({qi,qj});
-            query.push_back(qi);
-            query.push_back(qj);
-        }
-        sort(arr.begin(),arr.end());
-        sort(query.begin(),query.end());
-        //every thing is sorted
-        int j = 0,i=0;
-        while(j<2*q && i < n)
-        {
-            if(arr[i]<query[j] && fr[query[j]]==0)
-            {
-                rightDp[query[j]] = i;
-            }
-            if(arr[i]<=query[j])
-            {
-                rightDp[query[j]] = i;
-
-                i++;
-            }
-            else
-            {
-
-                j++;
-
-            }
-        }
-        j = 2*q-1,i=n-1;
-        while(j>=0 && i>= 0)
-        {
-            if(arr[i]>=query[j])   //8 >= 7 true leftDp[7] = 4
-            {
-                leftDp[query[j]] = i;
-                i--;
-            }
-            else
-            {
-                //current element is less then query[j]  move the query to left
-                j--;
-            }
-        }
-
-        //jesob element exist kore na tader same same query porle 0 ans asbe ..
-
-        cout << "Case " << cse++ << ":\n";
-        for(int i = 0; i < q; i++)
-        {
-            ll qi = queries[i].first, qj = queries[i].second;
-            if(qi==qj && fr[qi]==0)
-            {
-                cout << 0 << "\n";
-            }
-            else
-                cout << rightDp[qj] - leftDp[qi] + 1 << "\n" ; //this will not be working for an element that does not exist
-
+            cout << (upper_bound(arr,arr+n,qj)-arr)-(lower_bound(arr,arr+n,qi)-arr) << "\n";
         }
     }
-    return 0;
+
+
+return 0;
 }
 
 
 /**
 1
-5 3
-1 4 5 6 8 10
-0 5 6 7 10 10000
-dp[0] = 6
-dp[5] = 4 //including 5 in dp[5]
-dp[5] = 3 //excluding 5 from dp[5]
-
-scan and print should be used
-secondly ...
-there is an edge case i am missing surely .
-this program is jora tali dewa .. ..
-
-testcase
-1
-6 3
-1 4 5 6 8 10
+9 5
+1 4 5 5 6 6 6 8 10
 0 5
 6 10
 7 100000
-
-
-
+1000 1000
+0 0
 */
