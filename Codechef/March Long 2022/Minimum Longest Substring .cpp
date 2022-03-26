7ad96fbf713f57792ebb5c2da85e25c5687bc02d
dev-khalid <khalid.hossain.akash@gmail.com>
2022-03-27 00:49:02 +0600

    codechef march long

--- Codechef/March Long 2022/Minimum Longest Substring .cpp
+++ Codechef/March Long 2022/Minimum Longest Substring .cpp
@@ -1,18 +1,84 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main() {
+
+int main()
+{
     int t;
     cin >> t;
-    while(t--) {
+    while(t--)
+    {
         int n;
         cin >> n;
-        int ones = 0;
+        vector<char> s(n);
+        int zeros = 0,ones = 0;
         char c;
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++)
+        {
             cin >> c;
             if(c=='1') ones++;
+            else zeros++;
         }
-        int zeros = n-ones;
-        cout << abs(ones-zeros) << endl;
+        if(zeros>=ones)
+        {
+            for(int i = 0; i < n; i++) {
+                if(i&1 && ones) {
+                    printf("1");
+                    ones--;
+                } else {
+                    printf("0");
+                }
+            }
+            printf("\n");
+            for(int i = 0; i < n; i++) {
+                if(zeros) {
+                    printf("0");
+                    zeros--;
+                } else {
+                    printf("1");
+                }
+            }
+            printf("\n");
+        }
+        else
+        {
+            for(int i = 0; i < n; i++) {
+                if(i&1 && zeros) {
+                    printf("0");
+                    zeros--;
+                } else {
+                    printf("1");
+                }
+            }
+            printf("\n");
+            for(int i = 0; i < n; i++) {
+                if(ones) {
+                    ones--;
+                    printf("1");
+                } else {
+                    printf("0");
+                }
+            }
+            printf("\n");
+        }
     }
 }
+/*
+
+Input
+
+T
+6
+000001
+001000
+100000
+
+6
+001111
+
+6
+000111
+
+
+
+Output
+
