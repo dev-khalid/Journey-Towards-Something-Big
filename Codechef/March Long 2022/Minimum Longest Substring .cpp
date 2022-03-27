e2f579ca2adacb7e07e3744a3c77ecf2fd84bbe4
dev-khalid <khalid.hossain.akash@gmail.com>
2022-03-28 00:07:11 +0600

    Codechef March Long 2022 4 problem solved

--- Codechef/March Long 2022/Minimum Longest Substring .cpp
+++ Codechef/March Long 2022/Minimum Longest Substring .cpp
@@ -9,7 +9,6 @@ int main()
     {
         int n;
         cin >> n;
-        vector<char> s(n);
         int zeros = 0,ones = 0;
         char c;
         for(int i = 0; i < n; i++)
@@ -18,22 +17,95 @@ int main()
             if(c=='1') ones++;
             else zeros++;
         }
+        if(n==2 && zeros==ones)
+        {
+            cout << "01" << endl;
+            cout << "10" << endl;
+            continue;
+        }
+
         int x = max(zeros,ones);
         int y = min(zeros,ones);
+
+        char big=zeros>=ones?'0':'1',small=big=='0'?'1':'0';
+        int bigCnt = x,smallCnt = y;
+
+
+
         int seg;
-        if(y&1) {
+        if(y&1)
+        {
             seg = x/(y+1);
         }
-        else {
+        else
+        {
             //y is even
-            if(x&1){
+            if(x&1)
+            {
                 seg = ceil(x*1.0/(y+1));
-            } else {
+            }
+            else
+            {
                 seg = x/(y+1);
             }
         }
-        //now as i got my segment i need to find a filling strategy .
+        //check if y*2>=x ?
+        if(y*2>=x)
+        {
+            //put extra least elements in front line ..
+            int extra = 2*(y-x/2);//etogula o1 er segment se print korbe
+            //now run a loop for 2*extra
+            while(extra>0)
+            {
+                cout << big << small;
+                bigCnt--,smallCnt--;
+                extra--;
+            }
+            seg = 2;
 
+        }
+
+        //now as i got my segment i need to find a filling strategy .
+        while(bigCnt&&smallCnt)
+        {
+            int ss = seg;
+            while(ss&&bigCnt)
+            {
+                cout << big;
+                ss--,bigCnt-- ;
+            }
+            if(smallCnt)
+            {
+                cout << small;
+                smallCnt--;
+            }
+            //eibar eita kaj korbe thik thak moto but eikhane kichu extra element thakbe
+            //extra gulake
+        }
+        while(bigCnt)
+        {
+            cout << big;
+            bigCnt--;
+        }
+        while(smallCnt)
+        {
+            cout << small;
+            smallCnt--;
+        }
+
+        cout << "\n";
+        //We are now building second string
+        while(x)
+        {
+            cout << big;
+            x--;
+        }
+        while(y)
+        {
+            cout << small;
+            y--;
+        }
+        cout << "\n";
     }
 }
 /*
@@ -41,35 +113,16 @@ the trick is divide ones properly in stringOne
 and at stringTwo just keep all the elements segregated .
 Input
 
-4
-6
-000001
 
-6
-001111
-
-6
-000111
-
-6
-111110
-
-15
-000001000001001
-
-Output
-
-001000
-100000
-
-101011
-111100
-
-010101
-000111
-
-101111
-111110
+3
+23
+00000000000000001111111
+17
+00000000011111111
+17
+00000000001111111 -- found a mistake .
 
 
+
+should change my thinking process . but not more for today .
 */
