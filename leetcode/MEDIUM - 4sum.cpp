d173c28ed5ac705311d69b295f984cb91d572a48
dev-khalid <khalid.hossain.akash@gmail.com>
2022-07-26 19:28:18 +0600

    MEDIUM - LEETCODE

--- leetcode/MEDIUM - 4sum.cpp
+++ leetcode/MEDIUM - 4sum.cpp
@@ -1,24 +1,25 @@
 //source: https://leetcode.com/problems/4sum/
 #include<bits/stdc++.h>
 using namespace std;
-
+typedef long long ll;
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>> ans;
+        set<vector<int>> s;
         int n = nums.size();
         sort(nums.begin(),nums.end());
         for(int i = 0; i < n; i++) {
             for(int j = i+1; j < n; j++) {
                 //now go for two pointer approach
-                int target2 = target-nums[i]-nums[j];
+                ll target2 = (ll)target-nums[i]-nums[j];
                 int l = j+1,r=n-1;
                 while(l<r) {
                     int sum = nums[l]+nums[r];
                     if(sum==target2) {
                         vector<int> quad;
                         quad = {nums[i],nums[j],nums[l],nums[r]};
-                        ans.push_back(quad);
+                        s.insert(quad);
                         l++;
                     } else if(sum<target2) {
                         l++;
@@ -28,11 +29,16 @@ public:
                 }
             }
         }
+        for(auto it: s) {
+            ans.push_back(it);
+        }
         return ans;
     }
 };
 
 int main() {
+    vector<int> nums = {1,1,2,2,3,4};
+    int target = 8;
     Solution sol;
-
+    vector<vector<int>> res = sol.fourSum(nums,target);
 }
