2b91972e0b8e3dd035fd8d33d457469769c3428d
dev-khalid <khalid.hossain.akash@gmail.com>
2022-07-30 12:07:39 +0600

    MEDIUM LEETCODE

--- leetcode/MEDIUM - longest-substring-without-repeating-characters.cpp
+++ leetcode/MEDIUM - longest-substring-without-repeating-characters.cpp
@@ -1,3 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -23,4 +27,26 @@ public:
         }
         return ans;
     }
+    int efficient_algorthm(string s) {
+        map<char,int> lastPosition;
+        int n = s.size(), res = 0,j=0,i=-1;
+        for(; j < n; j++){
+            if(lastPosition.find(s[j]) != lastPosition.end()) {
+                //found duplicate
+                //increasing the window size
+                i = max(i,lastPosition[s[j]]+1);
+            }
+            res = max(res,j-i+1);
+            lastPosition[s[j]] = j;
+        }
+        return res;
+    }
 };
+
+
+int main() {
+    string s = "abcabcadda1234567890z";
+    Solution Sol;
+    cout << Sol.lengthOfLongestSubstring(s);
+    cout << Sol.efficient_algorthm(s);
+}
