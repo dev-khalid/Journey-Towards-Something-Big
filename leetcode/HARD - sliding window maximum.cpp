feeaa2e7fb09921de9f203cce6956616b75ef733
dev-khalid <khalid.hossain.akash@gmail.com>
2022-07-31 17:33:01 +0600

    BACS - GREEDY

--- leetcode/HARD - sliding window maximum.cpp
+++ leetcode/HARD - sliding window maximum.cpp
@@ -7,8 +7,34 @@ class Solution
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k)
     {
-        //we can do it using priority queue .
-        //there are several implementation we will
-        //need to implement that priority queue
+        queue<int> q;
+        vector<int> ans;
+        int n = nums.size();
+        int last = nums[0];
+        q.push(last);
+
+        for(int i = 0; i < k; i++) {
+            if(nums[i]<=last) {
+                q.push(nums[i]);
+                last = nums[i];
+            } else {
+                //we don't need previous elements. Safely remove those ele
+                while(!q.empty() || q.front()<= last) q.pop();
+                q.push(nums[i]);
+            }
+        }
+
+
     }
 };
+int main() {
+    Solution Sol;
+    vector<int> nums = {1,3,-1,-3,5,3,6,7};
+    int k = 3;
+    vector<int> ans;
+    ans = Sol.maxSlidingWindow(nums,k);
+    for(auto el: ans) {
+        cout << el << " ";
+    }
+    return 0;
+}
--- leetcode/MEDIUM - longest-substring-without-repeating-characters.cpp
+++ leetcode/MEDIUM - longest-substring-without-repeating-characters.cpp
@@ -2,22 +2,30 @@
 using namespace std;
 
 
-class Solution {
+class Solution
+{
 public:
-    int lengthOfLongestSubstring(string s) {
-        if(!s.size()) {
+    int lengthOfLongestSubstring(string s)
+    {
+        if(!s.size())
+        {
             return 0;
         }
         map<char,int> mp;
         int n = s.size();
         int ans = 1;
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++)
+        {
             int possible_ans = 0;
-            for(int j = i;  j < n; j++) {
-                if(mp[s[j]]) {
+            for(int j = i;  j < n; j++)
+            {
+                if(mp[s[j]])
+                {
                     ans = max(ans,possible_ans);
                     break;
-                } else {
+                }
+                else
+                {
                     mp[s[j]]++;
                     possible_ans++;
                 }
@@ -27,11 +35,14 @@ public:
         }
         return ans;
     }
-    int efficient_algorthm(string s) {
+    int efficient_algorthm(string s)
+    {
         map<char,int> lastPosition;
         int n = s.size(), res = 0,j=0,i=-1;
-        for(; j < n; j++){
-            if(lastPosition.find(s[j]) != lastPosition.end()) {
+        for(; j < n; j++)
+        {
+            if(lastPosition.find(s[j]) != lastPosition.end())
+            {
                 //found duplicate
                 //increasing the window size
                 i = max(i,lastPosition[s[j]]+1);
@@ -44,9 +55,17 @@ public:
 };
 
 
-int main() {
+int main()
+{
     string s = "abcabcadda1234567890z";
     Solution Sol;
     cout << Sol.lengthOfLongestSubstring(s);
     cout << Sol.efficient_algorthm(s);
 }
+
+
+
+
+
+
+
