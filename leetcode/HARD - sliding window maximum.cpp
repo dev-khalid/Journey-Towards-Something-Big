327c510400ae394d3d72c3b14149fc45237d7178
dev-khalid <khalid.hossain.akash@gmail.com>
2022-07-31 15:47:50 +0600

    BACS - GREEDY

---
+++ leetcode/HARD - sliding window maximum.cpp
@@ -0,0 +1,14 @@
+//source :- https://leetcode.com/problems/sliding-window-maximum/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k)
+    {
+        //we can do it using priority queue .
+        //there are several implementation we will
+        //need to implement that priority queue
+    }
+};
