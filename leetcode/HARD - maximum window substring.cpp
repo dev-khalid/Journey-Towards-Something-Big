8fc01fc6d940ac77a1997d388e30eca10f11db2f
dev-khalid <khalid.hossain.akash@gmail.com>
2022-08-07 21:58:31 +0600

    ALMOST DONE

--- leetcode/HARD - maximum window substring.cpp
+++ leetcode/HARD - maximum window substring.cpp
@@ -1,43 +1,128 @@
 //SOURCE:- https://leetcode.com/problems/minimum-window-substring/
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    bool matched(unordered_map<char,int> hash1,unordered_map<char,int> hash2) {
-        for(auto it: hash2) {
-            if(hash1[it.first] != it.second) {
+    bool matched(unordered_map<char,int> hash1,unordered_map<char,int> hash2)
+    {
+        for(auto it: hash2)
+        {
+            if(hash1[it.first] < it.second)
+            {
                 return false;
             }
         }
         return true;
     }
-    string minWindow(string s, string t) {
+    string minWindow(string s, string t)
+    {
         unordered_map<char,int> hash1,hash2;
-        string ans = "",build_helper="";
-        //find the first one .
+        string ans="",build_helper="";
+
+
         int n = s.size(),m = t.size();
-        for(auto c: t) {
+
+        if(n<m || !n || !m)
+        {
+            return ans;
+        }
+
+        //find the first one .
+        for(auto c: t)
+        {
             hash2[c]++;
         }
         int i;
-        for(i = 0; i < n;  i++) {
+        for(i = 0; i < n;  i++)
+        {
             build_helper += s[i];
             hash1[s[i]]++;
-            if(matched(hash1,hash2)) {
+            if(matched(hash1,hash2))
+            {
                 ans = build_helper;
                 break;
             }
         }
 
+        //removing some of the unnecessary elements from the front
+        if(ans.size()==0) return ans;
+        int j=0;
+        while(1)
+        {
+            if(hash2.find(build_helper[j])==hash2.end())
+                hash1.erase(build_helper[j++]);
+            else {
+                if(hash1[build_helper[j]]>hash2[build_helper[j]]) {
+                    hash1[build_helper[j]]--;
+                    j++;
+                } else {
+                    break;
+                }
+            }
+
+        };
+
+        build_helper = build_helper.substr(j);
+        if(ans.size()>build_helper.size()) ans = build_helper;
+
+        //now its started .
+        for(i=i+1; i<n; i++)
+        {
+            build_helper+=s[i];
+
+            if(hash2.find(s[i])!=hash2.end() && hash1.find(s[i])!=hash1.end())
+            {
+                //start removing till the matched character
+                j = 0;
+
+                while(build_helper[j++]!=s[i])
+                {
+                    hash1.erase(build_helper[j-1]);
+                } ;
+
+                build_helper = build_helper.substr(j);
+
+
+                //a little bit more optimization can be done
+                j=0;
+                while(hash2.find(build_helper[j])==hash2.end())
+                {
+                    hash1.erase(build_helper[j]);
+                    j++;
+
+                };
+
+                build_helper = build_helper.substr(j);
+
+
+            }
+            else
+            {
+                hash1[s[i]]++;
+            }
+            if(matched(hash1,hash2))
+            {
+                if(ans.size()>build_helper.size())
+                {
+                    ans = build_helper;
+                }
+            }
+        }
 
         return ans;
     }
 };
-int main() {
-    Solution sol;
-    string s,t,ans;
-    s = "ADOBECODEBANC",t="ABC";
-    ans = sol.minWindow(s,t);
-    cout << ans << endl;
+int main()
+{
+    while(1)
+    {
+        Solution sol;
+        string s,t,ans;
+        cin >> s >> t;
+        ans = sol.minWindow(s,t);
+        cout << ans << endl;
+
+    }
 
 }
