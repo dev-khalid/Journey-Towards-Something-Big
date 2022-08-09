a452f57f5f63d8ac885a7fbb2a942325c7a1af30
dev-khalid <khalid.hossain.akash@gmail.com>
2022-08-09 17:54:27 +0600

    MEDIUM - LEETCODE

---
+++ leetcode/MEDIUM - House Robber.cc
@@ -0,0 +1,44 @@
+//source :- https://leetcode.com/problems/house-robber/
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int dp[100000][2] = {-1};
+    int func(vector<int> nums,int pos,int prev) {
+        if(pos>=nums.size()) {
+            return 0;
+        }
+        if(dp[pos][prev]!=-1) return dp[pos][prev];
+        if(prev==0) {
+            //previous one excluded
+
+            //including current element
+            int val1 = func(nums,pos+1,1)+nums[pos];
+
+            //excluding current element
+            int val2 = func(nums,pos+1,0);
+
+            return dp[pos][prev] = max(val1,val2);
+
+        } else {
+            //excluding current element is the only option
+            int val2 = func(nums,pos+1,0);
+            return dp[pos][prev] = val2;
+        }
+    }
+    int rob(vector<int>& nums)
+    {
+        memset(dp,-1,sizeof dp);
+        func(nums,0,0);
+        cout << dp[0][0];
+        return 0;
+    }
+};
+int main()
+{
+    Solution sol;
+    vector<int> nums = {5,5,10,100,10,5};
+
+    cout << sol.rob(nums);
+}
