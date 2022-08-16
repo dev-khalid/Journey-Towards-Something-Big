db8696ebfaec90d8f286a43cc54d1a714922deb0
dev-khalid <khalid.hossain.akash@gmail.com>
2022-08-16 06:12:14 +0600

    os cpu scheduling

--- CPU SCHEDULING ALGORITHMS.cc
+++ CPU SCHEDULING ALGORITHMS.cc
@@ -4,10 +4,84 @@ using namespace std;
 class Algorithms
 {
 public:
-    void prioritySchedulingWithAT() {
-        //Algorithm: Priority Scheduling Algorithm With Arrival Time
-        //Criteria : Priority (Higher the number Higher the priority)  ; Mode: Preemptive
+    int n;
+    int AT[50],BT[50],P[50],CT[50],TAT[50],WT[50],BT2[50];
+    struct ProcessInformation
+    {
+        int pid,priority,arrival,burst;
+    };
 
+    ProcessInformation Data[50];
+    void custom_swap(int x,int y)
+    {
+        ProcessInformation temp = Data[x];
+        Data[x] = Data[y];
+        Data[y] = temp;
+    }
+    void SortArrival(int n)
+    {
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = i+1; j <= n; j++)
+            {
+                if(Data[i].arrival > Data[j].arrival)
+                {
+                    custom_swap(i,j);
+                }
+            }
+        }
+    }
+    void prioritySchedulingWithAT()
+    {
+        //Algorithm: Priority Scheduling Algorithm With Arrival Time
+        //Criteria : Priority (Lower the number Higher the priority)  ; Mode: Preemptive
+        //need a separate class to implement all of this
+
+
+
+
+
+        cout << "Enter Number of processes: ";
+        cin >> n;
+        for(int i = 1; i <= n; i++)
+        {
+            cout << "Enter Process " << i << "'s Arrival Time and Burst Time and Priority: ";
+            cin >> AT[i] >> BT[i] >> P[i];
+            BT2[i] = BT[i];
+            Data[i].pid = i,Data[i].arrival = AT[i],Data[i].burst = BT[i];
+        }
+        SortArrival(n);
+        int time, cnt = n;
+        for(time = 0; cnt > 0; time++)
+        {
+            int job = FindJob(time,n);
+            if(job == -1) continue;
+            BT[job]--; //job will be executed by one unit of time
+            if (BT[job]==0)
+            {
+                cnt--;
+                CT[job] = time + 1; //why this time + 1?
+            }
+        }
+        double Avwt = 0,Avtat ;
+        for(int i = 1; i <= n; i++)
+        {
+            TAT[i] = CT[i] - AT[i];
+            WT[i] = TAT[i] - BT2[i];
+            Avwt += WT[i];
+            Avtat += TAT[i];
+
+        }
+        Avwt /= n;
+        Avtat /= n;
+        cout << "Process\tArrival Time\tBurst Time\t Turnaround Time\tWating Time\n\n";
+        for (int i = 1; i <= n; i++)
+        {
+            int id = Data[i].pid;
+            cout << id << "\t"<< AT[id] <<"\t"<<BT2[id]<<"\t"<<TAT[id]<<"\t"<<WT[id]<<endl;
+        }
+        cout << "Average waiting time : " << Avwt << endl;
+        cout << "Average turnaround time : " << Avtat << endl;
     }
     void firstFit()
     {
