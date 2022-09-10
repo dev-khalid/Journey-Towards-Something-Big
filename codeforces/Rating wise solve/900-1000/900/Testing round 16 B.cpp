4197937bdcee26dcd42628d7101d6b00666c027c
dev_khalid <khalidhossain727@gmail.com>
2022-09-11 05:54:15 +0600

    CODEFORCES 900 30

---
+++ codeforces/Rating wise solve/900-1000/900/Testing round 16 B.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int a1,b1,a2,b2;
+        cin >> a1 >> b1 >> a2 >>  b2 ;
+        bool ok = false;
+        if(a1==a2) {
+            if(b1+b2==a1) {
+                ok = true;
+            }
+        }
+        else if(a1==b2) {
+            if(b1+a2==a1) ok = true;
+
+        }
+        else if(b1==a2) {
+            if(a1+b2==b1) {
+                ok = true;
+            }
+        }
+        else if(b1==b2) {
+            if(a2+a1==b1) ok = true;
+        }
+
+        cout << (ok?"YES\n":"NO\n");
+    }
+
+}
