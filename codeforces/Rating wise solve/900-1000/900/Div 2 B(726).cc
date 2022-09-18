df00dd564a359c825c1305a248593ef97254ad94
dev-khalid <khalid.hossain.akash@gmail.com>
2022-09-18 07:15:24 +0600

    CODEFORCES 900 30

---
+++ codeforces/Rating wise solve/900-1000/900/Div 2 B(726).cc
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin  >> t;
+    while(t--) {
+        int x,y,i,j;
+        cin >> x >> y >> i >> j;
+        cout << 1 << " " << 1 << " " << x << " " << y << endl;
+    }
+}
