de0fa2d70d3a0a3403ea977bc68eee51171622f7
dev-khalid <khalid.hossain.akash@gmail.com>
2022-10-02 11:39:43 +0600

    900-1000

---
+++ codeforces/Rating wise solve/900-1000/1000/Edu binary search/theory 2  rectangle .cc
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int min_required(int a,int b,int n) {
+    return min(a,b)*n;
+}
+bool available(int a,int b,int n,int grid) {
+    int total_available = 0;
+    total_available = (grid/a)*(grid/b);
+    return total_available >= n;
+}
+int main() {
+    int a,b;
+    cin >> a >> b;
+    int n;
+    cin >> n;
+    int l = a*b, h = min_required(a,b,n);
+    int m;
+    int ans = h;
+
+    while(l <= h) {
+        m = l + (h-l)/2;
+        if(available(a,b,n,m)) {
+            ans = min(ans,m);
+            h = m-1;
+        } else {
+            l = m+1;
+        }
+    }
+    cout << ans << endl;
+}
