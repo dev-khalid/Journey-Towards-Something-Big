1a7cfebcecbb3691a7fa424925df8c8e596f1bc0
dev-khalid <khalid.hossain.akash@gmail.com>
2022-10-20 09:39:18 +0600

    900-1000

--- codeforces/Rating wise solve/900-1000/1000/div 2 A(782).cc
+++ codeforces/Rating wise solve/900-1000/1000/div 2 A(782).cc
@@ -1,47 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
+int main() {
     int t;
     cin >> t;
-    while(t--)
-    {
-        int n;
-        cin >> n;
-        int r,b;
-        cin >> r >> b;
-        int ss = r/b;
-        int segS,rest;
-        //this is the maximum size of the segment
-        for(int i = 1; i <= ss; i++)
-        {
-            //keep a gap of i and print
-            //calculate kora dorkar je emon korle remaining elements koyta thake .
-            int covered = b*i+i;
-            int rs = r-covered;
-            cout << covered << " " << rs  << endl;
-            if(rs>=0 && rs<= i) {
-                rest = rs;
-                segS = i;
-                break;
-            }
-        }
-        cout << segS << " " << rest << endl;
-        /*
-        for(int _ = 0; _ < b; _++)
-        {
-            for(int i = 0; i < segS; i++)
-            {
+    while(t--) {
+        int n,r,b;
+        cin >> n >> r >> b;
+        int ss = r/(b+1);
+        int rest = r%(b+1);
+        for(int i = 0; i <= b; i++) {
+            for(int j = 0; j < ss; j++) {
                 cout << 'R';
             }
-            cout << 'B';
-        }
-        cout << rest << endl;
-        while(rest--)
-        {
-            cout << 'R';
+            if(rest) {
+                cout << 'R';
+                rest--;
+            }
+            if(i!=b) {
+                cout << 'B';
+            }
         }
         cout << endl;
-        */
     }
 }
