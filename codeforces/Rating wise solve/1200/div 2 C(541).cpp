d443f63b14daa4ff03ebc2c92409d80e07cf0d83
dev-khalid <khalid.hossain.akash@gmail.com>
2022-12-17 08:12:31 +0600

    1200

--- codeforces/Rating wise solve/1200/div 2 C(541).cpp
+++ codeforces/Rating wise solve/1200/div 2 C(541).cpp
@@ -1,1 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for(int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+        sort(v.begin(),v.end());
+        deque<int> d;
+        //if there is at least 2 elements then pick only one from that ...
+        for(int i = 0; i < n; i++) {
+             if(i&1) {
+                cout << v[i] << " ";
+             }
+             else {
+                d.push_front(v[i]);
+             }
+        }
+        for(auto it: d) {
+            cout << it << " ";
+        }
+        cout << endl;
+}
 
