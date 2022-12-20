ddf7abd3e171380bac6ea09eebf9558881438d5e
dev-khalid <khalid.hossain.akash@gmail.com>
2022-12-20 15:12:02 +0600

    1200

--- codeforces/Rating wise solve/1200/test1200.cc
+++ codeforces/Rating wise solve/1200/test1200.cc
@@ -1,20 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll calc(ll c,ll n)
-{
-    if(c==(n-1) || c==n) {
-        return 0;
-    }
-    return n-1-c;
-}
 int main()
 {
-    vector<ll> v(3);
-    cin >> v[0] >> v[1] >> v[2];
+    int n,l;
+    cin >> n >> l;
+    vector<double> v(n);
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    if(n==1) {
+        //this can be in the middle also . 1 10 3
+        cout << max(v[0],l-v[0]) << endl;
+        return 0;
+    }
     sort(v.begin(),v.end());
-    ll n = v[2];
-    ll ans = 0;
-    ans = calc(v[1],v[2]) + calc(v[0],v[2]);
-    cout << ans << endl;
+    double mx = 0;
+    for(int i = 1; i <n; i++) {
+        mx = max(mx,v[i]-v[i-1]);
+    }
+    double ans = (mx/2.0);
+
+    if(v[0]!=0) {
+        ans = max(ans,v[0]);
+    }
+    if(v[n-1]!=l) {
+        ans = max(ans,l-v[n-1]);
+    }
+    printf("%0.10lf\n",ans);
 }
