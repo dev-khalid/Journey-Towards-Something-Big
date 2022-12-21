a2a68133189ec8e544c95572b87f7a2ef45a8097
dev_khalid <khalidhossain727@gmail.com>
2022-12-21 11:17:09 +0600

    updated

--- codeforces/Rating wise solve/1200/div 2 B(271).cpp
+++ codeforces/Rating wise solve/1200/div 2 B(271).cpp
@@ -1,1 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<ll> a(n,0);
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if(i>0)
+        {
+            a[i]+= a[i-1];
+        }
+    }
+    int m;
+    cin >> m;
+    while(m--)
+    {
+        ll q;
+        cin >> q;
 
+        ll lb = lower_bound(a.begin(),a.end(),q)-a.begin();
+        lb++;
+        cout << lb << endl;
+    }
+}
