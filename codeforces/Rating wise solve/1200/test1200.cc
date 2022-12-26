957b27ffa8c96c633a957240890f08a7fa26d007
dev-khalid <khalid.hossain.akash@gmail.com>
2022-12-26 21:45:20 +0600

    1200

--- codeforces/Rating wise solve/1200/test1200.cc
+++ codeforces/Rating wise solve/1200/test1200.cc
@@ -1,45 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+int xFinder(int n) {
+    int down = sqrt(n-1)+1;
+    int nearestFullSquare = down*down;
+    int need = nearestFullSquare-n;
+    return need;
+}
 int main()
 {
     int t;
     cin >> t;
     while(t--)
     {
-        string s;
-        cin >> s;
-        vector<int> cnt(10,0),lp(10,-1);
-        int i = 1;
-        for(auto it: s) {
-            cnt[it-'0']++;
-            lp[it-'0'] = i++;
-        }
-        int currentPosition = 1;
-        for(auto it: s) {
-            int currElement = (it-'0');
-
-            if(cnt[currElement]<=0) {
-                continue;
-            }
-            //cout << "current element : position =  " << currElement  << currentPosition << endl;
-            for(i = 0; i < currElement; i++) {
-                //cout << "Last Position of : " << i << " " << lp[i] << endl;
-                if(lp[i]>currentPosition && cnt[i]) {
-                    cnt[currElement]--;
-                    int digit = min(currElement+1,9);
-                    cnt[digit]++;
-                    lp[digit] = max(lp[digit],currentPosition);
-                    //cout << "Enter korche ? ";
-                    break;
+        int n;
+        cin >> n;
+        //now solve for n
+        int prevN = n;
+        vector<int> v;
+        while(1) {
+            int x = xFinder(prevN-1);
+            if(x==0) {
+                for(int i = 0; i < prevN; i++) {
+                    v.push_back(i);
                 }
+                break;
             }
-            currentPosition++;
+            int y = x;
+            while(x<prevN) {
+                v.push_back(x++);
+            }
+            prevN = y;
+
+
+            //cout << "working: " << prevN << endl;
         }
-        for(int el = 0; el < 10; el++) {
-            for(i = 0; i < cnt[el]; i++) {
-                cout << el;
-            }
+        reverse(v.begin(),v.end());
+        for(auto it: v) {
+            cout << it << " ";
         }
         cout << endl;
     }
