eb8d3d8c0acea627b50df542fc3df945868be6e2
dev-khalid <khalid.hossain.akash@gmail.com>
2022-12-28 10:59:35 +0600

    1200

--- codeforces/Rating wise solve/1200/test1200.cc
+++ codeforces/Rating wise solve/1200/test1200.cc
@@ -1,47 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef __int128 i128;
+vector<ll> p2;
+void preCalc() {
+    ll total = 1LL;
+    p2.push_back(total);
+    for(ll i = 1; i <= 50; i++) {
+        total*=2LL;
+        p2.push_back(total);
+    }
+
+}
 int main()
 {
+    preCalc();
     int t;
     cin >> t;
     while(t--)
     {
-        int n,x;
-        cin >> n >> x;
-        vector<int> v(n+1);
-        for(int i = 1; i <= n; i++)
+        ll h,p;
+        cin >> h >> p;
+        ll total = 0LL;
+        ll time = 0LL;
+        ll task = 1LL;
+        bool hasGreater = false;
+        for(ll i = 0; i < h; i++)
         {
-            cin >> v[i];
-        }
-        if(is_sorted(v.begin()+1,v.end()) or x*2<=n)
-        {
-            cout << "YES\n";
-            continue;
-        }
-        if(x==n)
-        {
-            cout << "NO\n";
-            continue;
-        }
-
-        vector<int> sorted = v;
-        sort(sorted.begin()+1,sorted.end());
-        int start = x+1;
-        int last = n-x;
-        int s = min(start,last)+1;
-        int l = max(start,last);
-        l = min(l,n);
-        s = max(s,0);
-        bool sortedFlag = true;
-        for(int i = s; i < l; i++) {
-            if(sorted[i]!=v[i]) {
-                sortedFlag = false;
-                break;
+            task = p2[i];
+            if(task>=p)
+            {
+                hasGreater = true;
+                total += task;
+                continue;
             }
+            time++;
         }
-        cout << (sortedFlag?"YES": "NO") << "\n";
-
-        }
+        if(hasGreater)
+            time+=(total-1LL)/p+1LL;
+        cout << time << endl;
+    }
 }
 
