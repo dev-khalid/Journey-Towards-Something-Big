0eaec8db012b385c78511920c74490d56a7cfcd2
dev-khalid <khalid.hossain.akash@gmail.com>
2023-01-02 10:00:51 +0600

    1200

--- codeforces/Rating wise solve/1200/test1200.cc
+++ codeforces/Rating wise solve/1200/test1200.cc
@@ -3,10 +3,22 @@ using namespace std;
 typedef long long ll;
 typedef __int128 i128;
 const ll MOD=1e9+7;
-ll permutation(ll n) {
+
+ll factorial(ll n)
+{
     ll result = 1LL;
-    for(ll i = 1; i <= n; i++) {
+    for(ll i = 1; i <= n; i++)
+    {
         result*=i;
+        result%=MOD;
+    }
+    return result;
+}
+ll permutation(ll n,ll r) {
+    ll result = 1LL;
+    ll limit = n-r;
+    for(; n > limit; n--) {
+        result*=n;
         result%=MOD;
     }
     return result;
@@ -15,14 +27,24 @@ int main()
 {
     int t;
     cin >> t;
-    while(t--) {
+    while(t--)
+    {
         ll n,k;
         cin >> n >> k;
-        ll x,y,z;
-        x = (1LL<<(k-1))-1LL;
-        y = x+1LL;
-        z = (1LL<<k)-1LL;
-        cout << permutation(20) << endl;
+        ll ans=0LL;
+        ans += permutation(n,1LL);
+        ll k2 = 1LL<<k;
+        k2/=2LL;
+
+        ll perm2 = permutation(n,2LL);
+        cout << ans << ": " << perm2 << endl;
+        for(int i = 0; i+1 < k2; i++) {
+            ans+=perm2;
+            ans%=MOD;
+        }
+
+        cout << ans << endl;
+
     }
 }
 /**
