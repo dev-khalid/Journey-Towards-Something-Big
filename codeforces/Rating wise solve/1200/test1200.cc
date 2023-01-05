696d6f548f94bd9dda54c4588289a54f2dd4bbc7
dev-khalid <khalid.hossain.akash@gmail.com>
2023-01-05 15:21:11 +0600

    1200

--- codeforces/Rating wise solve/1200/test1200.cc
+++ codeforces/Rating wise solve/1200/test1200.cc
@@ -3,49 +3,37 @@ using namespace std;
 typedef long long ll;
 typedef __int128 i128;
 const ll MOD=1e9+7;
-
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     int t;
     cin >> t;
     while(t--)
     {
         int n;
         cin >> n;
-
-        if(n==3)
+        vector<ll>x,y;
+        for(int i = 0; i < 2*n; i++)
         {
-            cout << "NO\n";
+            ll a,b;
+            cin >> a >> b;
+            if(a)
+            {
+                x.push_back(abs(a));
+            }
+            else
+            {
+                y.push_back(abs(b));
+            }
         }
-        else
+        sort(x.begin(),x.end());
+        sort(y.begin(),y.end());
+        double ans = 0;
+        for(int i = 0; i < n; i++)
         {
-            cout << "YES\n";
-            if(n&1)
-            {
-                int ans = n/2;
-
-                for(int i = 0; i < n; i++)
-                {
-                    if(i&1)
-                    {
-                        cout << ans<< " ";
-                    }
-                    else
-                    {
-                        cout << -1*(ans-1) << " ";
-                    }
-
-                }
-                cout << endl;
-                continue;
-            }
-            int ans = -1;
-            for(int i = 0; i < n; i++)
-            {
-                cout << ans << " ";
-                ans*=-1;
-            }
-            cout << endl;
+            ans+= sqrt(x[i]*x[i]+y[i]*y[i]);
         }
+        cout << fixed << setprecision(17) << ans << endl;
     }
 }
