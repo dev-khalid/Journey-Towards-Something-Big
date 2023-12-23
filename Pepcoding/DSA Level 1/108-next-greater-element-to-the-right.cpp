21abe637fb37426919d835ef772d41d6af2e2f00
dev-khalid <khalid.hossain.akash@gmail.com>
2023-12-23 15:36:23 +0600

    code refactoring

--- Pepcoding/DSA Level 1/108-next-greater-element-to-the-right.cpp
+++ Pepcoding/DSA Level 1/108-next-greater-element-to-the-right.cpp
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+/**
+    Practice link: https://practice.geeksforgeeks.org/problems/next-greater-element/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
+    Practice link: https://leetcode.com/problems/next-greater-element-ii/
+*/
 class Solution {
   public:
     vector<long long> nextLargerElement(vector<long long> &arr, int n){
@@ -11,23 +14,36 @@ class Solution {
         st.push(arr[n-1]); 
         n = n-1; 
         while(n--) {  
-            while(true) { 
-                //if the stack is empty then the ans will be -1 
-                if(st.empty()) { 
-                    st.push(arr[n]);
-                    result[n] = -1; 
-                    break; 
-                }
-                if(arr[n]>=st.top()) { 
-                    //On smaller elements just remove then ... 
-                    st.pop(); 
-                } else { 
-                    result[n] = st.top(); 
-                    st.push(arr[n]); 
-                    break; 
-                }
+            //Code refactoring... 
+            // while(true) { 
+            //     //if the stack is empty then the ans will be -1 
+            //     if(st.empty()) { 
+            //         st.push(arr[n]);
+            //         result[n] = -1; 
+            //         break; 
+            //     }
+            //     if(arr[n]>=st.top()) { 
+            //         //On smaller elements just remove then ... 
+            //         st.pop(); 
+            //     } else { 
+            //         result[n] = st.top(); 
+            //         st.push(arr[n]); 
+            //         break; 
+            //     }
+            // }
+
+            while(!st.empty() && arr[n] >= st.top()) { 
+                st.pop(); 
             }
+            if(st.empty()) { 
+                result[n] = -1; 
+            } 
+            else { 
+                result[n] = st.top(); 
+            }
+            st.push(arr[n]); 
         }
+
         return result; 
     }
 };
