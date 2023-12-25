dbafff453296f0ba8d0c97d0fb3657f5b94ac18f
dev-khalid <khalid.hossain.akash@gmail.com>
2023-12-25 19:17:16 +0600

    largest area histogram

--- Pepcoding/DSA Level 1/114-largest-area-histogram.cpp
+++ Pepcoding/DSA Level 1/114-largest-area-histogram.cpp
@@ -9,6 +9,57 @@ void printQ(deque<pair<int,int>> t) {
     }
     cout << endl; 
 }
+
+int main() { 
+    int n; 
+    cin >> n; 
+    vector<int> v(n); 
+    for(auto &it: v) cin >> it;
+    vector<int> l(n), r(n); 
+    stack<int> st; 
+    st.push(n-1); 
+    r[n-1] = n; 
+    for(int i = n-2; i >= 0; i--) { 
+        while(!st.empty() && v[st.top()] >= v[i]) {
+            //it will pop untill it finds next smaller element on it's right 
+            st.pop();
+        }
+        if(st.empty()) { 
+            r[i] = n; 
+        } else { 
+            r[i] = st.top(); 
+        }
+        st.push(i); 
+
+    }
+
+    st = stack<int>();
+    l[0] = -1;     
+    st.push(0);
+    for(int i = 1; i < n; i++) { 
+        while(!st.empty() && v[st.top()] >= v[i]) {
+            //it will pop untill it finds next smaller element on it's right 
+            st.pop();
+        }
+        if(st.empty()) { 
+            l[i] = -1; 
+        } else { 
+            l[i] = st.top(); 
+        }
+        st.push(i); 
+
+    }
+
+    int mx = 0; 
+    for(int i = 0; i < n; i++) { 
+        mx = max(mx,v[i]*(r[i]-l[i]-1)); 
+    }
+    cout << mx << endl; 
+
+}
+
+/**
+My implementation
 int main() {
     int n; 
     cin >> n; 
@@ -50,3 +101,5 @@ int main() {
     }
     cout << mx << endl; 
 }
+
+*/
